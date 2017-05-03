52440c8b64eed09ed7bcbe67a36004dcf4278e2f
heimtech <dominik@heim-tech.de>
2017-05-03 20:07:15 +0200

    update

--- scripts/fineloo_main.js
+++ scripts/fineloo_main.js
@@ -1,12 +1,23 @@
-jQuery(document).ready(function () {
+/*jQuery(document).ready(function () {
 
     jQuery(window).resize(function () {
-
+        var lNewHeight2 = jQuery(".finelooframe").contents().find("#FinelooBody").height();
         var lNewHeight = jQuery("#FinelooBody").height();
-        jQuery(".finelooframe").css( "height" ,lNewHeight );
+        jQuery(".finelooframe").css( "height" ,lNewHeight2 );
     });
+
+
+    jQuery('.finelooframe').on("load", function() {
+        var lNewHeight2 = jQuery(".finelooframe").contents().find("#FinelooBody").height();
+        var lNewHeight = jQuery("#FinelooBody").height();
+        jQuery(".finelooframe").css( "height" ,lNewHeight2 );
+    });
+
+
+
 });
 
 
 
 
+*/
